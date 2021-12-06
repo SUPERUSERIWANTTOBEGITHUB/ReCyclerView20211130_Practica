ca145c0a97d93c6af27ba79fa1a31459b2e66e8e
AS <yvg68@yandex.ru>
2021-12-06 18:30:55 +0300

    final DatesAdapter.kt

--- app/src/main/java/yvg68/measurer/recyclerview20211130_practica/DatesAdapter.kt
+++ app/src/main/java/yvg68/measurer/recyclerview20211130_practica/DatesAdapter.kt
@@ -1,15 +1,21 @@
 package yvg68.measurer.recyclerview20211130_practica
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class DatesAdapter(private val datesQuestionsList: List<DatesQuestioons>): RecyclerView.Adapter<DatesViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DatesViewHolder {
-        TODO("Not yet implemented")
+        val itemView =
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.user_list_item,parent, false)
+        return DatesViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: DatesViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val dateQuestions = datesQuestionsList[position]
+        holder.bind(dateQuestions)
+
     }
 
     override fun getItemCount(): Int {
